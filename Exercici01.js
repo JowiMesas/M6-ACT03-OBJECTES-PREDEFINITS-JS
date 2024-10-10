77e4125ed07ebfed85cb9f081a69babbe6c8b4a2
Jowi <157366219+JowiMesas@users.noreply.github.com>
2024-10-10 18:31:30 +0200

    act 03

---
+++ Exercici01.js
@@ -0,0 +1,12 @@
+let numeroAleatorio
+function play() {
+numeroAleatorio = Math.floor(Math.random() * 11);
+let divNumero = document.getElementById('numero');
+divNumero.textContent = numeroAleatorio;
+if(numeroAleatorio < 5) {
+    divNumero.style.color = 'red';
+} else {
+    divNumero.style.color = 'green';
+}
+window.open('encerta.html', 'Encerta', 'width=800,height=600');
+}
---
+++ encerta.js
@@ -0,0 +1,14 @@
+let intents = 0;
+function incrementarIntents() {
+    intents++;
+}
+function compara() {
+    let inputNumero = document.getElementById('inputNumero').value;
+    let divMissatge = document.getElementById('missatge');
+    let numeroAleatorio1 = window.opener.numeroAleatorio;
+
+    incrementarIntents();
+    if(inputNumero === numeroAleatorio1) {
+        
+    }
+}
