33fe3eec3e604d928f61f61bf644f22e233edf40
Jowi <157366219+JowiMesas@users.noreply.github.com>
2024-11-07 19:17:59 +0100

    encerta

--- encerta.js
+++ encerta.js
@@ -7,11 +7,12 @@ function compara() {
     let divMissatge = document.getElementById('missatge');
     let numeroAleatorio1 = parseInt(window.opener.document.getElementById("numero").textContent);
     console.log(numeroAleatorio1);
-
-    incrementarIntents();
     if(inputNumero === numeroAleatorio1) {
-        
+        alert("Has encertat el numero!");
+    } else {
+        alert("Es incorrecte");
     }
+    
 }
 function setCookie(cname, cvalue, exdays) {
     const d = new Date();
